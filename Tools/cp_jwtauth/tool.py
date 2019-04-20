1ee36b0092931e232efefc3d45ff3418d97fd372
Karl Lattimer <karl@qdh.org.uk>
2019-04-20 14:48:04 +0100

    ..

--- Tools/cp_jwtauth/tool.py
+++ Tools/cp_jwtauth/tool.py
@@ -62,5 +62,39 @@ class JWTAuthTool(cherrypy.Tool):
             return
 
         # Get the username/password from URL
-        # Get the username/password from request JSON
-        # Get the username/password from
+
+        request = cherrypy.serving.request
+
+        auth_header = request.headers.get('authorization')
+        if auth_header is not None:
+            # split() error, base64.decodestring() error
+            msg = 'Bad Request'
+            with cherrypy.HTTPError.handle((ValueError, binascii.Error), 400, msg):
+                scheme, params = auth_header.split(' ', 1)
+                if scheme.lower() == 'basic':
+                    charsets = accept_charset, fallback_charset
+                    decoded_params = base64.b64decode(params.encode('ascii'))
+                    decoded_params = _try_decode(decoded_params, charsets)
+                    decoded_params = ntou(decoded_params)
+                    decoded_params = unicodedata.normalize('NFC', decoded_params)
+                    decoded_params = tonative(decoded_params)
+                    username, password = decoded_params.split(':', 1)
+
+                    if self.auth_mech.checkpass(username, password):
+                        request.login = username
+                        return  # successful authentication
+
+            charset = accept_charset.upper()
+            charset_declaration = (
+                ('charset="%s"' % charset)
+                if charset != fallback_charset
+                else ''
+            )
+            # Respond with 401 status and a WWW-Authenticate header
+            cherrypy.serving.response.headers['www-authenticate'] = (
+                'Basic %s' % (realm, charset_declaration)
+            )
+        raise cherrypy.HTTPError(401, 'You are not authorized to access that resource')
+
+            # Get the username/password from request JSON
+            # Get the username/password from
